4fad759ee15aff6a49008f65d9966e104862160c
Slobodan Adamovic <slobodan@studiopresent.com>
2023-09-11 09:51:30 +0200

    making custom pwa

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   // register: true,
+//   // scope: '/app',
+//   // sw: 'service-worker.js',
+//   //...
+// });
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["i.dummyjson.com"],
   },
-});
+};
 
 module.exports = nextConfig;
