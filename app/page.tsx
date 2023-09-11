b69e93e1f523450942355995491d6cd02e90e6ea
Slobodan Adamovic <slobodan@studiopresent.com>
2023-09-11 13:56:44 +0200

    added code for insttall prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
+"use client";
 import { getProducts } from "@/services";
 import { Product } from "@/types";
+import { use, useEffect } from "react";
 import Card from "./components/Card";
 import styles from "./page.module.css";
-export default async function Home() {
-  const products = await getProducts();
+export default function Home() {
+  const products = use(getProducts());
+
+  useEffect(() => {
+    let deferredPrompt: any;
+
+    function showInstallButton() {
+      console.log("ulazi vode");
+      const installButton = document.getElementById("install-button")!;
+      installButton.style.display = "block";
+
+      installButton.addEventListener("click", () => {
+        // Show the installation prompt
+        deferredPrompt.prompt();
+
+        // Wait for the user to make a choice
+        deferredPrompt.userChoice.then((choiceResult: any) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the installation prompt");
+          } else {
+            console.log("User dismissed the installation prompt");
+          }
+
+          // Reset the deferredPrompt variable
+          deferredPrompt = null;
+
+          // Hide the install button
+          installButton.style.display = "none";
+        });
+      });
+    }
+
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the browser's default prompt from showing up
+      event.preventDefault();
+
+      // Store the event for later use
+      deferredPrompt = event;
+
+      // Show your custom "Install" button or other UI element
+      showInstallButton();
+    });
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1>PWA demo</h1>
@@ -14,6 +58,9 @@ export default async function Home() {
           </li>
         ))}
       </ul>
+      <button id="install-button" style={{ display: "none" }}>
+        Install App
+      </button>
     </main>
   );
 }
