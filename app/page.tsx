713f5713c41c2bbc1d7314792b86107f8fbbe53f
Slobodan Adamovic <slobodan@studiopresent.com>
2023-09-11 15:25:42 +0200

    added code for prompt in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,35 @@
 "use client";
 import { getProducts } from "@/services";
 import { Product } from "@/types";
-import { use, useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import Card from "./components/Card";
 import styles from "./page.module.css";
 export default function Home() {
   const products = use(getProducts());
-
+  const [showButton, setShowButton] = useState(true);
+  const [prompt, setPrompt] = useState<any>();
   useEffect(() => {
-    let deferredPrompt;
+    const handle_storePrompt = (e: any) => {
+      e.preventDefault();
+      if (showButton) setPrompt(e);
+    };
 
-    window.addEventListener("beforeinstallprompt", (e) => {
-      console.log("e", e);
-      deferredPrompt = e;
-    });
-  }, []);
+    window.addEventListener("beforeinstallprompt", (e) =>
+      handle_storePrompt(e)
+    );
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", (e) =>
+        handle_storePrompt(e)
+      );
+    };
+  }, [showButton]);
+
+  const handle_prompt = (_) => {
+    setShowButton(false);
+    prompt.prompt();
+    setPrompt(null);
+  };
   return (
     <main className={styles.main}>
       <h1>Pwa demo</h1>
@@ -25,6 +40,12 @@ export default function Home() {
           </li>
         ))}
       </ul>
+      <button
+        style={{ display: showButton ? "block" : "none" }}
+        onClick={handle_prompt}
+      >
+        <small>Click to Install</small>
+      </button>
     </main>
   );
 }
