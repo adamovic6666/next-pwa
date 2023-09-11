3bcae1f2bcba264ab3065dabd4bad39679bc8ba4
Slobodan Adamovic <slobodan@studiopresent.com>
2023-09-11 15:19:20 +0200

    changed manifest file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { getProducts } from "@/services";
 import { Product } from "@/types";
+import { use, useEffect } from "react";
 import Card from "./components/Card";
 import styles from "./page.module.css";
-export default async function Home() {
-  const products = await getProducts();
+export default function Home() {
+  const products = use(getProducts());
+
+  useEffect(() => {
+    let deferredPrompt;
+
+    window.addEventListener("beforeinstallprompt", (e) => {
+      console.log("e", e);
+      deferredPrompt = e;
+    });
+  }, []);
   return (
     <main className={styles.main}>
       <h1>Pwa demo</h1>
