80d36d10a2796cc99b62ee34a97c2339c6ae7d97
Slobodan Adamovic <slobodan@studiopresent.com>
2023-09-12 13:15:41 +0200

    added modal for app installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+"use client";
 import { getProducts } from "@/services";
 import { Product } from "@/types";
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import styles from "./page.module.css";
-export default async function Home() {
-  const products = await getProducts();
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e: any) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+  }, []);
+
+  useEffect(() => {
+    const getProductsHandler = async () => {
+      setProducts(await getProducts());
+    };
+
+    getProductsHandler();
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt?.prompt();
+      deferredPrompt?.userChoice?.then((choiceResult: any) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt");
+        } else {
+          console.log("User dismissed the install prompt");
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
 
   return (
     <main className={styles.main}>
@@ -15,6 +47,13 @@ export default async function Home() {
           </li>
         ))}
       </ul>
+      <button
+        id="installButton"
+        onClick={handleInstallClick}
+        style={{ display: deferredPrompt ? "block" : "none" }}
+      >
+        Install
+      </button>
     </main>
   );
 }
